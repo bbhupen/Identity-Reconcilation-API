5a1d31b30b6364f069ba230c62833a9a8e8ebc90
bbhupen <bbhupen601@gmail.com>
2023-07-03 20:03:47 +0530

    add cyclic deployment

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -6,7 +6,12 @@ const router = express.Router()
 router.post("/api/v1/identify", async (req,res) => {
     const requestBody = req.body;
     const response = await identifyUser(requestBody);
-    res.send(response);
+
+    if (!response || Object.keys(response).length === 0) {
+        return res.status(404).json({ error: 'Unexpected error occurred'});
+      }
+
+    res.status(200).json(response);
 
 })
 
